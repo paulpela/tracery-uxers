90cdd58b9ddd9e7135c58b131682a2dc3ba821b2
Paweł Pela <paulpela@gmail.com>
2019-03-11 10:05:31 +0100

    updates

--- js/app.js
+++ js/app.js
@@ -1,29 +1,12 @@
 console.log("app");
 
 jQuery(document).ready(function($) {
-    var maleNames = (function () {
-        var json = null;
-        jQuery.ajax({
-            'async': false,
-            'global': false,
-            'url': 'https://raw.githubusercontent.com/paulpela/corpus/master/people/names/male-names.json',
-            'dataType': "json",
-            'success': function (data) {
-                json = data;
-            }
-        });
-        return json;
-    })(); 
-
-    var input = {
-        "avatar": [ "<div class=\"row mb-4\"><div class=\"col-3\"><img class=\"avatar\" src=\"https://miro.medium.com/fit/c/240/240/1*lwkv9EOqIU3Q_NGuTc1C9Q@2x.jpeg\"></div><div class=\"col-9\"><h3>#maleName# Surname, <small>UX Designer</small></h3><p>@funny_username</p></div></div>" ],
-        "origin": [ "#avatar#<p><strong>#maleName#</strong> is a beginner UX designer living in Budapest, Hungary. He's 30 years old. He used to struggle with wireframing in the past. One day he discovered new tools and read some articles and decided to improve his workflow. He worked hard to achieve success, until finally he managed to optimise X.</p><p><strong>Personality:</strong> He is energetic, smart and outgoing. He's often lazy.</p><p>His Twitter username is @funny_username</p><p>#loremIpsum#</p>" ],
-        "maleName": maleNames,
-        "loremIpsum": [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in mauris quis odio dictum pulvinar ac ut dolor. Aliquam in tincidunt eros. Sed in dapibus turpis. Sed aliquet arcu ac sollicitudin viverra. In vel lacus eget felis laoreet vulputate. Sed placerat sem vel scelerisque varius. Donec ullamcorper consequat gravida. Morbi lobortis arcu neque, ut mattis nisl scelerisque ut. Nulla bibendum non sapien vitae ullamcorper. Nam neque augue, tincidunt nec auctor eleifend, sodales ut purus. Suspendisse a ornare lorem. Nulla facilisi. Sed sed sem elementum risus euismod efficitur et hendrerit ante." ]
-    };
 
     function loadGrammar() {
-        jQuery("#output").html("");
+        jQuery("#generate").prop("disabled", true);
+        jQuery("#generate i").addClass('fa-spin');
+
+        jQuery("#output").html("<div class=\"loading\"><i class=\"fas fa-3x fa-spinner fa-spin\"></i></div>");
 
         var grammar = tracery.createGrammar(input);
         jQuery("#grammar").html(grammar.toText());
@@ -49,11 +32,12 @@ jQuery(document).ready(function($) {
             return json;
         })(); 
 
+        jQuery("#output").html("");
         jQuery("#output").append(div);
         jQuery("#output img").attr("src", avatar);
 
-        $("#generate i").removeClass('fa-spin');
-
+        jQuery("#generate i").removeClass('fa-spin');
+        jQuery("#generate").prop("disabled", false);
     }
 
     setTimeout(function() {
@@ -64,9 +48,6 @@ jQuery(document).ready(function($) {
     jQuery('#survey').tooltip({ offset: 10 });
 
     jQuery('#generate').click(function() {
-        $("#generate").prop("disabled", true);
-        $("#generate i").addClass('fa-spin');
         loadGrammar();
-        $("#generate").prop("disabled", false);
     });
 });
---
+++ js/grammars.js
@@ -0,0 +1,21 @@
+
+var maleNames = (function () {
+    var json = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'url': 'https://raw.githubusercontent.com/paulpela/corpus/master/people/names/male-names.json',
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})(); 
+
+var input = {
+    "avatar": [ "<div class=\"row mb-4\"><div class=\"col-3\"><img class=\"avatar\" src=\"https://miro.medium.com/fit/c/240/240/1*lwkv9EOqIU3Q_NGuTc1C9Q@2x.jpeg\"></div><div class=\"col-9\"><h3>#maleName# Surname, <small>UX Designer</small></h3><p>@funny_username</p></div></div>" ],
+    "origin": [ "#avatar#<p><strong>#maleName#</strong> is a beginner UX designer living in Budapest, Hungary. He's 30 years old. He used to struggle with wireframing in the past. One day he discovered new tools and read some articles and decided to improve his workflow. He worked hard to achieve success, until finally he managed to optimise X.</p><p><strong>Personality:</strong> He is energetic, smart and outgoing. He's often lazy.</p><p>His Twitter username is @funny_username</p><p>#loremIpsum#</p>" ],
+    "maleName": maleNames,
+    "loremIpsum": [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in mauris quis odio dictum pulvinar ac ut dolor. Aliquam in tincidunt eros. Sed in dapibus turpis. Sed aliquet arcu ac sollicitudin viverra. In vel lacus eget felis laoreet vulputate. Sed placerat sem vel scelerisque varius. Donec ullamcorper consequat gravida. Morbi lobortis arcu neque, ut mattis nisl scelerisque ut. Nulla bibendum non sapien vitae ullamcorper. Nam neque augue, tincidunt nec auctor eleifend, sodales ut purus. Suspendisse a ornare lorem. Nulla facilisi. Sed sed sem elementum risus euismod efficitur et hendrerit ante." ]
+};
